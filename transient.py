390f8e1fea911435da2f5240a49a25893f98e7df
Aaron VanSledright <avansledright@gmail.com>
2020-03-06 12:43:21 -0500

    Updated for sys.argv input rather than changing script

--- transient.py
+++ transient.py
@@ -2,8 +2,13 @@ import datetime
 import mysql.connector
 import sys
 
+DB_USERNAME = sys.argv[1]
+DB_ADDRESS = sys.argv[2]
+DB_NAME = sys.argv[3]
+DB_USER_PASSWORD = sys.argv[4]
+
 # define variables
-cnx = mysql.connector.connect(user='DB_USERNAME', host='DB_ADDRESS', database='DB_NAME', password='DB_USER_PASSWORD')
+cnx = mysql.connector.connect(user=DB_USERNAME, host=DB_ADDRESS, database=DB_NAME, password=DB_USER_PASSWORD)
 cursor = cnx.cursor()
 now = datetime.datetime.now()
 query = ("DELETE FROM `wp_options` WHERE `autoload` = 'yes' AND `option_name` LIKE '%transient%';")
@@ -16,6 +21,6 @@ cursor.execute(query)
 #commit changes
 cnx.commit()
 
-print now, "Deleted", cursor.rowcount, "lines."
+print(now, "Deleted", cursor.rowcount, "lines.")
 
 cnx.close
